73d42420001df626058a0852df7b02a630682215
haraa1 <hariscolakhodzic1@gmail.com>
2025-03-02 23:40:20 +0100

    add base services and controllers

---
+++ eCinema/eCinema.Services/IService.cs
@@ -0,0 +1,15 @@
+﻿using eCinema.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCinema.Services
+{
+    public interface IService<T, TSearch> where TSearch : class
+    {
+        Task<PagedResult<T>> Get(TSearch search = null);
+        Task<T> GetById(int id);
+    }
+}
