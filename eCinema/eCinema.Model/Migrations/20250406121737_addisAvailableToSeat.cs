a83de782716bd4a98f52cb1a01822cf06b0d6d6a
haraa1 <hariscolakhodzic1@gmail.com>
2025-04-10 23:57:31 +0200

    migration: add isAvailable to Seat

---
+++ eCinema/eCinema.Model/Migrations/20250406121737_addisAvailableToSeat.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eCinema.Models.Migrations
+{
+    /// <inheritdoc />
+    public partial class addisAvailableToSeat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isAvailable",
+                table: "Seats",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isAvailable",
+                table: "Seats");
+        }
+    }
+}
